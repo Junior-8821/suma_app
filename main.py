773cd38df3c4290b0947272f5ddf24fb59270984
junior <juniorcollado123@gmail.com>
2024-03-26 12:45:29 -0400

    Espacio

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ if __name__ == "__main__":
     num1 = float(input("Ingrese el primer número: "))
     num2 = float(input("Ingrese el segundo número: "))
     print("La suma es:", sumar(num1, num2))
+
